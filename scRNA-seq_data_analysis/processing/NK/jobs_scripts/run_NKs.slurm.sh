efd0c27278265aac49e7676e2f4ca1f8f88e8ae7
VicenteFR <vfajardo@lji.org>
2023-07-05 13:23:18 -0700

    Snakefiles ready for processing of RNA-seq data

--- scRNA-seq_data_analysis/processing/NK/jobs_scripts/run_NKs.slurm.sh
+++ scRNA-seq_data_analysis/processing/NK/jobs_scripts/run_NKs.slurm.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-#SBATCH --job-name=DICET_CD4s
+#SBATCH --job-name=DICET_NKs
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=5g
 #SBATCH --time=120:00:00
-#SBATCH --output=/mnt/bioadhoc-temp/Groups/vd-vijay/vfajardo/R24/paper_developments/R24_Cancer/github_repo/DICE_Lung/scRNA-seq_data_analysis/processing/CD4/jobs_scripts/run_CD4s.out.txt
-#SBATCH --error=/mnt/bioadhoc-temp/Groups/vd-vijay/vfajardo/R24/paper_developments/R24_Cancer/github_repo/DICE_Lung/scRNA-seq_data_analysis/processing/CD4/jobs_scripts/run_CD4s.err.txt
+#SBATCH --output=/mnt/bioadhoc-temp/Groups/vd-vijay/vfajardo/R24/paper_developments/R24_Cancer/github_repo/DICE_Lung/scRNA-seq_data_analysis/processing/NK/jobs_scripts/run_NKs.out.txt
+#SBATCH --error=/mnt/bioadhoc-temp/Groups/vd-vijay/vfajardo/R24/paper_developments/R24_Cancer/github_repo/DICE_Lung/scRNA-seq_data_analysis/processing/NK/jobs_scripts/run_NKs.err.txt
 
 source activate snakemake
-cd /mnt/bioadhoc-temp/Groups/vd-vijay/vfajardo/R24/paper_developments/R24_Cancer/github_repo/DICE_Lung/scRNA-seq_data_analysis/processing/CD4
+cd /mnt/bioadhoc-temp/Groups/vd-vijay/vfajardo/R24/paper_developments/R24_Cancer/github_repo/DICE_Lung/scRNA-seq_data_analysis/processing/NK
 
-WORKDIR=/mnt/bioadhoc-temp/Groups/vd-vijay/vfajardo/R24/paper_developments/R24_Cancer/github_repo/DICE_Lung/scRNA-seq_data_analysis/processing/CD4
+WORKDIR=/mnt/bioadhoc-temp/Groups/vd-vijay/vfajardo/R24/paper_developments/R24_Cancer/github_repo/DICE_Lung/scRNA-seq_data_analysis/processing/NK
 LOGS_PATH=${WORKDIR}/logs/
 
 snakemake -p --jobs 10 --latency-wait 60 --snakefile ${WORKDIR}/jobs_scripts/Snakefile --configfile ${WORKDIR}/configs/config.yaml --cluster-config ${WORKDIR}/configs/cluster.json --cluster "sbatch --time={cluster.walltime} --nodes=1 --ntasks=1 --cpus-per-task={cluster.cores} --mem={cluster.memory} -e ${LOGS_PATH}/{rule}.{jobid}.err.txt -o ${LOGS_PATH}/{rule}.{jobid}.out.txt --export ALL --parsable" --stats ${LOGS_PATH}/snakemake.stats >& ${LOGS_PATH}/snakemake.log --rerun-incomplete --rerun-triggers mtime --cluster-status /mnt/bioadhoc-temp/Groups/vd-vijay/Cristian/DICE_LungCancer/eQTL_pipeline/pipelines/pipeline_DICE/smk-slurm.sh
