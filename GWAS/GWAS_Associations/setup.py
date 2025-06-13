485eee8f6fa11d7749cdc3fc09794efbcf9e5d55
jh-rocha <90658320+jh-rocha@users.noreply.github.com>
2025-06-13 15:39:42 -0700

    Add files via upload

---
+++ GWAS/GWAS_Associations/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='R24',
+    version='0.0.2',
+    packages=["GWAS", "LiftOver", "Coloc"],
+    url='',
+    license='',
+    package_dir = {"GWAS": "R24/GWAS", "LiftOver": "R24/LiftOver", "Coloc": "R24/Coloc"},
+    author='jrocha',
+    author_email='jrocha@lji.org',
+    description=''
+)
